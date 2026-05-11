fa4571c4b0472b5dc0f65a198812e28c5c8ff993
smaldos64 <gudumholm34@gmail.com>
2026-05-11 13:42:58 +0200

    Udviklet eksemplet.

--- Interface3/Program.cs
+++ Interface3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Interface3.Classes;
 using Interface3.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Interface4_Automatiseret_Test
 {
@@ -8,49 +9,51 @@ namespace Interface4_Automatiseret_Test
     {
         static void Main(string[] args)
         {
-            //CreditCardProcessor creditCardProcessor_Object = new CreditCardProcessor();
-            //MobilePayProcessor mobilePayProcessor_Object = new MobilePayProcessor();
-            //EMailService eMailService_Object = new EMailService();
-
-            //CheckoutManager checkoutManager_Object = new CheckoutManager(creditCardProcessor_Object);
-            //CheckoutManager checkoutManager_Object1 = new CheckoutManager(mobilePayProcessor_Object);
-                        
             // 1. VI BYGGER CONTAINEREN (Konfiguration)
             var serviceProvider = new ServiceCollection()
                 // Vi registrerer vores klasser præcis som i Web API
-                //.AddScoped<MobilePayProcessor>()
-                //.AddScoped<IPaymentProcessor>(sp => sp.GetRequiredService<MobilePayProcessor>())
-                //.AddScoped<INotifier>(sp => sp.GetRequiredService<MobilePayProcessor>())
-                //.AddScoped<CheckoutManager>()
-                //.BuildServiceProvider(); // Her "låses" containeren
-
-                //.AddScoped<MobilePayProcessor>()
-                //.AddScoped<IPaymentProcessor>(sp => sp.GetRequiredService<MobilePayProcessor>())
-                //.AddScoped<EMailService>()
-                //.AddScoped<INotifier>(sp => sp.GetRequiredService<EMailService>())
-                //.AddScoped<CheckoutManager>()
-                //.BuildServiceProvider(); // Her "låses" containeren
-
+                .AddScoped<MobilePayProcessor>()
                 .AddScoped<CreditCardProcessor>()
-                .AddScoped<IPaymentProcessor>(sp => sp.GetRequiredService<CreditCardProcessor>())
                 .AddScoped<EMailService>()
-                .AddScoped<INotifier>(sp => sp.GetRequiredService<EMailService>())
+                .AddScoped<IPaymentProcessor>(sp => sp.GetRequiredService<MobilePayProcessor>())
+                .AddScoped<INotifier>(sp => sp.GetRequiredService<MobilePayProcessor>())
+                
+                .AddScoped<IPaymentProcessor, CreditCardProcessor>()
+                .AddScoped<INotifier, EMailService>()
                 .AddScoped<CheckoutManager>()
                 .BuildServiceProvider(); // Her "låses" containeren
 
             // 2. VI HENTER STARTOBJEKTET
             // I en konsol-app skal vi manuelt bede om det første objekt (vores indgangsvinkel)
-            CheckoutManager manager_Object = serviceProvider.GetRequiredService<CheckoutManager>();
+            var manager = serviceProvider.GetRequiredService<CheckoutManager>();
 
             // 3. BRUG AF KODEN
-            manager_Object.CompleteOrder(150.00m);
+            manager.CompleteOrder(150.00m);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dynamisk håndtering !!!");
             Console.WriteLine("");
 
-            manager_Object.CompleteOrder(250.00m);
-            Console.WriteLine("");
+            // SCENARIE 1: Alt-i-én (MobilePay)
+            var services1 = new ServiceCollection();
+            services1.AddScoped<IPaymentProcessor, MobilePayProcessor>();
+            services1.AddScoped<INotifier, MobilePayProcessor>();
+            services1.AddScoped<CheckoutManager>();
+            var provider1 = services1.BuildServiceProvider();
 
-            CheckoutManager manager_Object1 = serviceProvider.GetRequiredService<CheckoutManager>();
-            manager_Object1.CompleteOrder(2000.00m);
+            // SCENARIE 2: Kun betaling (SimpleCardReader)
+            var services2 = new ServiceCollection();
+            services2.AddScoped<IPaymentProcessor, CreditCardProcessor>();
+            services2.AddScoped<INotifier, EMailService>();
+            services2.AddScoped<CheckoutManager>();
+            var provider2 = services2.BuildServiceProvider();
+
+            // AFVIKLING
+            Console.WriteLine("--- Scenarie 1: MobilePay ---");
+            provider1.GetRequiredService<CheckoutManager>().CompleteOrder(100);
+
+            Console.WriteLine("\n--- Scenarie 2: CreditCardProcessor og EMAilService ---");
+            provider2.GetRequiredService<CheckoutManager>().CompleteOrder(100);
 
             Console.ReadKey();
         }
